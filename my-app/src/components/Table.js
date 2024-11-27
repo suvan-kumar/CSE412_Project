4a95e177b9241446ff85a19ce0a6684af93ec049
Waley Lin <94070836+WaleyLin@users.noreply.github.com>
2024-11-27 11:30:57 -0700

    Create Table.js

---
+++ my-app/src/components/Table.js
@@ -0,0 +1,24 @@
+function Table({ columns, data }) {
+  return (
+    <table border="1">
+      <thead>
+        <tr>
+          {columns.map((column, index) => (
+            <th key={index}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {Object.values(row).map((cell, cellIndex) => (
+              <td key={cellIndex}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
