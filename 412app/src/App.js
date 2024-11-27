d9ea3ba95afa85778ba6a32f1c731610922c4440
Waley Lin <94070836+WaleyLin@users.noreply.github.com>
2024-11-27 13:14:38 -0700

    Update App.js

--- 412app/src/App.js
+++ 412app/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import TeamList from "./pages/TeamList";
+import ManagerList from "./pages/ManagerList";
+import PlayerList from "./pages/PlayerList";
+import StatsList from "./pages/StatsList";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to 412App</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teams" element={<TeamList />} />
+        <Route path="/managers" element={<ManagerList />} />
+        <Route path="/players" element={<PlayerList />} />
+        <Route path="/stats" element={<StatsList />} />
+      </Routes>
+    </Router>
   );
 }
 
