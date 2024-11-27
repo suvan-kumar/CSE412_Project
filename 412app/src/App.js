ac39eccf31ff5603461d50e67c40b1d8f2a1e457
Waley Lin <94070836+WaleyLin@users.noreply.github.com>
2024-11-27 13:22:43 -0700

    Update App.js

--- 412app/src/App.js
+++ 412app/src/App.js
@@ -1,20 +1,28 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import TeamList from "./pages/TeamList";
-import ManagerList from "./pages/ManagerList";
-import PlayerList from "./pages/PlayerList";
-import StatsList from "./pages/StatsList";
+import "./App.css"; // Import app-specific styles
+import Navbar from "./components/Navbar"; // Navbar component
+import Home from "./pages/Home"; // Home page
+import TeamList from "./pages/TeamList"; // Teams page
+import ManagerList from "./pages/ManagerList"; // Managers page
+import PlayerList from "./pages/PlayerList"; // Players page
+import StatsList from "./pages/StatsList"; // Stats page
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/teams" element={<TeamList />} />
-        <Route path="/managers" element={<ManagerList />} />
-        <Route path="/players" element={<PlayerList />} />
-        <Route path="/stats" element={<StatsList />} />
-      </Routes>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/teams" element={<TeamList />} />
+            <Route path="/managers" element={<ManagerList />} />
+            <Route path="/players" element={<PlayerList />} />
+            <Route path="/stats" element={<StatsList />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
